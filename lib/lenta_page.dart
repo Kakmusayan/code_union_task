ef895960bfc8afc8d376a9b62e292479b68e32de
Kakmusaya <scadaengineercs@gmail.comm>
2023-07-07 20:03:07 +0600

    add pages

---
+++ lib/lenta_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class LentaPage extends StatelessWidget {
+  const LentaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
